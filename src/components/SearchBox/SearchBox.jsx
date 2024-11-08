6da6a088272b379113aaa7b70ce60f0c315e11a6
antonina <tonya.garkava@gmail.com>
2024-11-08 09:31:23 +0100

    change icon

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,25 @@
+
+import css from "./SearchBox.module.css";
+
+const SearchBox = ({
+  filterValue,
+  handleFilter,
+}) => {
+  return (
+    <div className={css.container}>
+      <label className={css.label}>
+        Find contacts by name:
+        <input
+          className={css.inputClassName}
+          type="text"
+          value={filterValue}
+          onChange={(event) =>
+            handleFilter(event.target.value)
+          }
+        />
+      </label>
+    </div>
+  );
+};
+
+export default SearchBox;
